ffdc503576321f5c72ffd0de8a6d26938d87311f
Samuel Sherrer <samuel-sherrer@hotmail.com>
2021-10-25 13:43:16 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from 'react';
+import { BiCalendar } from 'react-icons/bi';
+import Search from './components/Search'
+import AddAppointment from './components/AddAppointment';
+import AppointmentInfo from './components/AppointmentInfo';
 
 function App() {
+
+  let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
+
+  const filteredAppointments = appointmentList.filter(item => {
+    return (
+      item.petName.toLowerCase().includes(query) ||
+      item.ownerName.toLowerCase().includes(query) ||
+      item.aptNotes.toLowerCase().includes(query)
+    )
+  }).sort((a, b) => {
+    let order = (orderBy === "asc") ? 1 : -1;
+
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
+    );
+  });
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data);
+      })
+  }, []);
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-200 align-top" />
+        Your Appointments</h1>
+      <AddAppointment />
+      <Search query={query}
+        onQueryChange={myQuery => {
+          setQuery(myQuery);
+        }}
+        sortBy={sortBy}
+        onSortByChange={mySort => { setSortBy(mySort) }}
+        orderBy={orderBy}
+        onOrderByChange={myOrder => { setOrderBy(myOrder) }} />
+      <ul className="divide-y divide-gray-200">
+        {filteredAppointments.map(appointment => (
+          <AppointmentInfo key={appointment.id}
+            appointment={appointment}
+            onDeleteAppointment={appointmentId =>
+              setAppointmentList(appointmentList.filter(appointment => appointment.id !== appointmentId))
+            } />
+        ))}
+      </ul>
     </div>
   );
 }
